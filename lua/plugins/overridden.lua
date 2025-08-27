f979fe60c65acdc52c0f6df21ad4add31c4a382a
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2025-08-27 09:18:04 -0300

    fix: foi removido o buffer do autocomplete

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -1,4 +1,17 @@
 return {
+  {
+    "Saghen/blink.cmp",
+    opts = {
+      sources = {
+        providers = {
+          path = { score_offset = 3 },
+          lsp = { score_offset = 0 },
+          snippets = { score_offset = -1 },
+          -- buffer = { score_offset = -3 },
+        },
+      },
+    },
+  },
   { "akinsho/toggleterm.nvim", config = function() require("toggleterm").setup { direction = "float" } end },
   {
     "L3MON4D3/LuaSnip",
